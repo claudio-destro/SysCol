0f654693be43bd2ff0bd350b06f223d7b70b4bc1
Claudio Destro <claudio.destro@gmail.com>
2023-08-23 19:22:59 +0200

    Remove SerialPort mock

--- src/script/openSerialPort.ts
+++ src/script/openSerialPort.ts
@@ -1,15 +1,11 @@
 import {SerialPort} from "serialport";
 import {parseSerialPortOpenOptions} from "./parseSerialPortOpenOptions";
-import {env} from "node:process";
-import {EchoSerialPortMock} from "./EchoSerialPortMock";
 
 export const openSerialPort = async (path: string, args: string): Promise<SerialPort> => {
-  const options = {
+  const serialPort = new SerialPort({
     ...parseSerialPortOpenOptions(args),
     autoOpen: false,
     path,
-  };
-  const mock = env["NODE_ENV"] === "development";
-  const serialPort = mock ? new EchoSerialPortMock(options).asSerialPort() : new SerialPort(options);
+  });
   return new Promise((resolve, reject) => serialPort.open(err => (err ? reject(err) : resolve(serialPort))));
 };
