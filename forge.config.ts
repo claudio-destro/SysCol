bd78ee46abb2f716488cd2f533932a1dcef8178e
Claudio Destro <claudio.destro@gmail.com>
2023-08-25 18:55:08 +0200

    Update package.json dependencies and Electron Forge build script

--- forge.config.ts
+++ forge.config.ts
@@ -5,31 +5,20 @@ import {MakerDeb} from "@electron-forge/maker-deb";
 import {MakerRpm} from "@electron-forge/maker-rpm";
 import {AutoUnpackNativesPlugin} from "@electron-forge/plugin-auto-unpack-natives";
 import {WebpackPlugin} from "@electron-forge/plugin-webpack";
-import {PathOrFileDescriptor, readFileSync} from "node:fs";
 import {spawn} from "node:child_process";
-import * as path from "node:path";
 
 import {mainConfig} from "./webpack.main.config";
 import {rendererConfig} from "./webpack.renderer.config";
 
-const readJson = <T>(path: PathOrFileDescriptor): T => JSON.parse(readFileSync(path).toString());
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const readPackageJson = async (_forgeConfig: ResolvedForgeConfig, packageJson: Record<string, any>): Promise<Record<string, any>> => {
-  if (Object.keys(packageJson.dependencies).length === 0) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const originalPackageJson: Record<string, any> = readJson(path.resolve(__dirname, "package.json"));
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const webpackConfigJs = require("./webpack.renderer.config.js");
-    Object.keys(webpackConfigJs["externals"]).forEach(pkg => (packageJson.dependencies[pkg] = originalPackageJson.dependencies[pkg]));
-  }
-  delete packageJson["scripts"];
-  return packageJson;
+  const {scripts, ...pkg} = packageJson;
+  return pkg;
 };
 
 const packageAfterPrune = async (_forgeConfig: ResolvedForgeConfig, buildPath: string): Promise<void> => {
   return new Promise((resolve, reject) => {
-    const npmInstall = spawn("npm", ["install", "--omit=dev"], {
+    const npmInstall = spawn("npm", ["install", "--omit", "dev"], {
       cwd: buildPath,
       stdio: "ignore",
       shell: true,
@@ -38,7 +27,7 @@ const packageAfterPrune = async (_forgeConfig: ResolvedForgeConfig, buildPath: s
       if (code === 0) {
         resolve();
       } else {
-        reject(new Error("process finished with error code " + code));
+        reject(new Error(`Npm exited with error ${code} `));
       }
     });
     npmInstall.on("error", reject);
