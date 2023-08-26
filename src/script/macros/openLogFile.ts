7f3cda034306833e65d8cb5b05916bfdfc16527d
Claudio Destro <claudio.destro@gmail.com>
2023-08-26 17:04:49 +0200

    Improve log file writer

--- src/script/macros/openLogFile.ts
+++ src/script/macros/openLogFile.ts
@@ -3,21 +3,34 @@ import {dirname, resolve} from "node:path";
 import {createWriteStream} from "node:fs";
 import {LogFile} from "../LogFile";
 import {TestScript} from "../TestScript";
+import {randomInt} from "crypto";
 
 const now = (): string => new Date().toISOString().replace(/[-:]|\.\d+/g, "");
+const rnd = (): string => "" + randomInt(0, 1_000_000);
+
+const mungeFileName = (name: string): string => {
+  return name.replace(/\{\{([^}]+)}}/g, ($0, $1): string => {
+    switch ($1) {
+      case "rnd":
+        return rnd();
+      case "now":
+        return now();
+      default:
+        return $0;
+    }
+  });
+};
+
+const prefix = (prefix: string, maxLength = 5, fillString = " "): string => prefix.padStart(maxLength, fillString);
+
+const instant = (microseconds: number): string => `[${(microseconds / 1000).toFixed(1)}ms]`;
 
 export const openLogFile = async (script: TestScript, logFile: string): Promise<LogFile> => {
   const {filePath: basePath} = script;
   const basedir = typeof basePath === "string" || basePath instanceof Buffer ? dirname(basePath.toString()) : app.getPath("logs");
-  const path = resolve(
-    basedir,
-    logFile.replace(/\{\{([^}]+)}}/g, ($0, $1): string => ($1 === "datetime" ? now() : $0)),
-  );
+  const path = resolve(basedir, mungeFileName(logFile))
   const writer = createWriteStream(path);
-
-  const prefix = (prefix: string, maxLength = 5, fillString = " "): string => prefix.padStart(maxLength, fillString);
-  const instant = (microseconds: number): string => `[${(microseconds / 1000).toFixed(1)}ms]`;
-
+  
   const onCommand = (command: string) => writer.write(`${prefix("<<")} ${command}\r\n`);
   const onResponse = (response: string, elapsed: number) => writer.write(`${prefix(">>")} ${response} ${instant(elapsed)}\r\n`);
   const onTest = (response: string, passed: boolean, elapsed: number) => writer.write(`${prefix(passed ? "PASS" : "FAIL")} ${response} ${instant(elapsed)}\r\n`);
@@ -30,20 +43,21 @@ export const openLogFile = async (script: TestScript, logFile: string): Promise<
   script.on("command", onCommand);
   script.on("response", onResponse);
   script.on("test", onTest);
+
+  const close = (): void => {
+    writer.close();
+    script.off("error", onError);
+    script.off("message", onMessage);
+    script.off("command", onCommand);
+    script.off("response", onResponse);
+    script.off("test", onTest);
+  }
+
   return {
-    get path(): string {
-      return path;
-    },
+    path,
     write(message: string): void {
       writer.write(message);
     },
-    close(): void {
-      writer.close();
-      script.off("error", onError);
-      script.off("message", onMessage);
-      script.off("command", onCommand);
-      script.off("response", onResponse);
-      script.off("test", onTest);
-    },
+    close,
   };
 };
