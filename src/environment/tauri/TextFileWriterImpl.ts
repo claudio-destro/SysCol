9ab248388a279ef0cf24a3de4b45a1a5f344517e
Claudio Destro <claudio.destro@gmail.com>
2023-09-09 22:18:48 +0200

    Update Tauri interface

--- src/environment/tauri/TextFileWriterImpl.ts
+++ src/environment/tauri/TextFileWriterImpl.ts
@@ -1,9 +1,11 @@
-import {TextFileWriter} from "../TextFileWriter";
+import EventEmitter from "eventemitter3";
+import {TextFileWriter, TextFileWriterEvents} from "../TextFileWriter";
 import {BaseDirectory, writeTextFile} from "@tauri-apps/api/fs";
 
 export class TextFileWriterImpl implements TextFileWriter {
   readonly #chunks: Array<string> = [];
   readonly #filePath: string;
+  readonly #events = new EventEmitter<string>();
 
   constructor(file: string) {
     this.#filePath = file;
@@ -13,15 +15,23 @@ export class TextFileWriterImpl implements TextFileWriter {
     return this.#filePath;
   }
 
-  onclose = (): void => {
-    /* EMPTY */
-  };
+  on<T extends keyof TextFileWriterEvents>(event: T, listener: TextFileWriterEvents[T]): void {
+    this.#events.on(event, listener);
+  }
+
+  once<T extends keyof TextFileWriterEvents>(event: T, listener: TextFileWriterEvents[T]): void {
+    this.#events.once(event, listener);
+  }
+
+  off<T extends keyof TextFileWriterEvents>(event: T, listener?: TextFileWriterEvents[T]): void {
+    this.#events.off(event, listener);
+  }
 
   async close(): Promise<void> {
     const path = this.filePath;
     const data = this.#chunks.join("");
     this.#chunks.length = 0;
-    return writeTextFile(path, data, {dir: BaseDirectory.Document}).finally(this.onclose);
+    return writeTextFile(path, data, {dir: BaseDirectory.Document}).finally(() => this.#events.emit("close"));
   }
 
   async write(chunk: string): Promise<void> {
