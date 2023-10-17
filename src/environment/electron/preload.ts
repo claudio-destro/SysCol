0d7503f30c0e2e33f97d1ed2e6e121a6d622d547
Claudio Destro <claudio.destro@gmail.com>
2023-10-17 07:22:26 +0200

    Sonar lint

--- src/environment/electron/preload.ts
+++ src/environment/electron/preload.ts
@@ -5,7 +5,7 @@ import {SysColApi} from "./SysColApi";
 type RegisteredEventMap = {
   [event in IpcRendererEvent]: Array<{
     callback: IpcRendererEventListenerMap[event];
-    ipcListener: (event: Electron.IpcRendererEvent, responseChannel: string, errorChannel: string, ...parameters: Parameters<IpcRendererEventListenerMap[event]>) => void;
+    ipcListener: (event: Electron.IpcRendererEvent, responseChannel: string, errorChannel: string, ...parameters: Parameters<IpcRendererEventListenerMap[event]>) => Promise<void>;
   }>;
 };
 
@@ -37,13 +37,13 @@ const SYS_COL_API: SysColApi = {
       }
     };
     REGISTERED_EVENTS[event].push({callback, ipcListener});
-    ipcRenderer.on(event, ipcListener);
+    ipcRenderer.on(event, ipcListener); // NOSONAR typescript:S654
   },
   unregisterEventListener<E extends IpcRendererEvent>(event: E, callback: IpcRendererEventListenerMap[E]): void {
     REGISTERED_EVENTS[event]
       .filter(({callback: cb}) => callback === cb)
       .forEach(({ipcListener}) => {
-        ipcRenderer.off(event, ipcListener);
+        ipcRenderer.off(event, ipcListener); // NOSONAR typescript:S654
         // FIXME remove from registry
       });
   },
